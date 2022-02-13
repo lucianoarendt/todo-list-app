cb869a0f23346a48097cdf991978bd3560790d76
Rafael Marinho <rafael_mf3@hotmail.com>
2022-02-13 00:50:08 -0300

    #7: add project controller

--- backend/controllers/projects/controller.go
+++ backend/controllers/projects/controller.go
@@ -1,6 +1,7 @@
 package projects
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -62,11 +63,75 @@ func (t *projectServiceImpl) Create(c *fiber.Ctx) error {
 }
 
 func (t *projectServiceImpl) Read(c *fiber.Ctx) error {
-	return nil
+	claims, err := middleware.Auth(t.secret, c)
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	var project []models.Project
+	if err := database.DB.Debug().
+		Where("projects.owner=?", claims.Issuer).
+		Find(&project); err != nil {
+		fmt.Println(err)
+	}
+
+	for idx, i := range project {
+		if err := database.DB.Debug().
+			Where("tasks.project=?", strconv.Itoa(int(i.ID))).
+			Find(&project[idx].Tasks); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	return c.JSON(project)
 }
+
 func (t *projectServiceImpl) Update(c *fiber.Ctx) error {
-	return nil
+	var data models.Project
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	_, err := middleware.Auth(t.secret, c)
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	var project models.Project
+	database.DB.Debug().Where("id=?", data.ID).Find(&project)
+	project.Title = data.Title
+	database.DB.Save(&project)
+
+	return c.JSON(project)
 }
 func (t *projectServiceImpl) Delete(c *fiber.Ctx) error {
-	return nil
+	id := c.Query("id")
+
+	_, err := middleware.Auth(t.secret, c)
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	var project models.Project
+	if err := database.DB.Debug().Delete("owner=?", id); err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "error on delete project",
+		})
+	}
+
+	return c.JSON(project)
 }
