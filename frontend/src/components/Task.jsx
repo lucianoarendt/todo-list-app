c420919cc0a114a9b571a4eb3187ebc4067eb392
Rafael Marinho <rafael_mf3@hotmail.com>
2022-02-16 09:53:13 -0300

    biz rules

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -28,7 +28,8 @@ export const Task = (props) => {
   }
 
   const updateStatus = async () => {
-    const response = await axios.put('http://localhost:8000/api/task/update', {
+    if (props.item.status == 1) { return }
+    await axios.put('http://localhost:8000/api/task/update', {
       status: 1,
     },{
       params: {id: props.item.ID},
@@ -36,6 +37,16 @@ export const Task = (props) => {
     });
     props.deleted();
   }
+
+  let timer1 = setTimeout(() => setShow(false), 5000)
+  useEffect(
+    () => {
+      return () => {
+        clearTimeout(timer1)
+      }
+    },
+    [show]
+  )
 
   return (
     <ListGroup className="list-group-flush">
@@ -48,36 +59,49 @@ export const Task = (props) => {
         ref={target} onClick={() => setShow(!show)}
       >
         <>
-          <GoCalendar/>
-          <Overlay target={target.current} show={show} placement="left">
-            {(props) => (
-              <Tooltip id="overlay-example" {...props}>
-                {props.edit.}
+          <GoCalendar style={{marginRight: 15}}/>
+          <Overlay 
+            target={target.current} 
+            show={show} 
+            placement="left"
+            onClick={timer1}
+          >
+            {(evs) => (
+              <Tooltip id="overlay-example" {...evs}>
+                {props.item.status == 1
+                  ? `concluded at: ${(new Date(props.item.UpdatedAt).toLocaleString('pt-BR'))}`
+                  : `created at: ${(new Date(props.item.CreatedAt).toLocaleString('pt-BR'))}`
+                }
+                
               </Tooltip>
             )}
           </Overlay>
         </>
         <Form.Check 
+          checked={props.item.status == 1}
           aria-label="??" 
           onClick={updateStatus}
-          onChange={updateStatus}
         />
-        {props.edit == props.item.ID
+        {props.edit == props.item.ID && props.item.status != 1
           ? <Form.Control
-            style={{marginLeft: 15, marginRight: 15}}
-            value={desc}
-            onChange={e => setDesc(e.target.value)}
-            onMouseOut={updateDesc}
-            onDoubleClick={() => props.handleEdit('')}
-          >
-          </Form.Control>
+              style={{marginLeft: 15, marginRight: 15}}
+              value={desc}
+              onChange={e => setDesc(e.target.value)}
+              onMouseOut={updateDesc}
+              onDoubleClick={() => props.handleEdit('')}
+            />
           : <Form.Text 
-          style={{margin: 15, fontSize: 18, flex: 2}}
+          style={{
+            margin: 15, 
+            fontSize: 18, 
+            flex: 2,
+            textDecoration: props.item.status == 1 ? 'line-through' : ''
+          }}
           onClick={() => props.handleEdit(props.item.ID)}  
         >{props.item.description}</Form.Text>
         }
         {
-          trash ? <GoTrashcan onClick={removeTask}/> : <></>
+          (trash && props.item.status != 1) ? <GoTrashcan onClick={removeTask}/> : <></>
         }
         
       </Container>
